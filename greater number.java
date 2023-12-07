0cbec0d17365492a10485e3e61242d413c148ad4
Git-Akila <118921510+Git-Akila@users.noreply.github.com>
2023-12-07 20:47:07 +0530

    Create greater number.java

---
+++ greater number.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class akila
+{
+    public static void main(String args[])
+    {
+        Scanner aki=new Scanner(System.in);
+        System.out.println("Enter the numbers:");
+        int a=aki.nextInt();
+        int b=aki.nextInt();
+        int c=aki.nextInt();
+        if(a>b && a>c)
+        {
+            System.out.println("A is greater");
+        }
+        else if(b>a && b>c){
+            System.out.println("B is greater");
+        }
+        else {
+            System.out.println("C is greater");
+        }
+
+    }
+}
