def3ee5e90a026590417fda78ef893753dcb954e
Git-Akila <118921510+Git-Akila@users.noreply.github.com>
2023-12-31 10:35:04 +0530

    Create fuction using_adding.java
    first -> function creation
    secomd->function content
    third-> main function creation
    fourth->function calling

---
+++ fuction using_adding.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class akila{
+    public static int add(int a,int b){
+        return a+b;
+    }
+    public static void main(String args[])
+    { int a=6,b=3;
+        System.out.println("After adding:"+add(a,b));
+    }
+}
